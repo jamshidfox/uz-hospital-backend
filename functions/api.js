6a6be6708f659d5aa48662b7a6a789433529cfa6
Jamshidbek <jamanu1910077@gmail.com>
2023-04-12 00:03:44 +0500

    check one more time

--- functions/api.js
+++ functions/api.js
@@ -4,33 +4,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const serverless = require('serverless-http');
 const { authorize } = require('../route/functions');
-const WebSocket = require('ws');
-const port = 9999;
+
 //Cors
 app.use(cors());
 //Socket
-const server = app.listen(port, () => {
-  console.log('Server started');
-});
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-  console.log('A new user connected');
-
-  ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
-
-    // Echo the message back to the client
-    ws.send(`You said: ${message}`);
-  });
-});
-
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
-  });
-});
 
 //
 //Route
